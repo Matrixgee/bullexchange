94a3593ac403721f21a5b9037c72c92259e8fd58
Dhanielgeek <dhanielknightz46@gmail.com>
2025-06-26 22:54:57 +0100

    test...

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -11,23 +11,22 @@ import { setAdminToken } from "../Global/AdminSlice";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     userName: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent) => {
-e.preventDefault();
+    e.preventDefault();
 
     setLoading(true);
     const loadingId = toast.loading("Please wait");
@@ -36,6 +35,8 @@ e.preventDefault();
       console.log(res);
 
       const userId = res.data.data._id;
+
+      // VITE_DEVE_URL = https://bme-c0yv.onrender.com/api
 
       toast.success("Login Successful");
 
@@ -62,7 +63,7 @@ e.preventDefault();
       setLoading(false);
       toast.dismiss(loadingId);
     }
-  }
+  };
 
   return (
     <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-900/90 via-black/90 to-black p-6">
@@ -111,7 +112,6 @@ e.preventDefault();
             </button>
           </div>
 
-
           <div className="text-right">
             <Link
               to="/forgot-password"
@@ -127,9 +127,8 @@ e.preventDefault();
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            {
-              loading ? (
-                <svg
+            {loading ? (
+              <svg
                 className="animate-spin h-5 w-5 text-white"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -148,9 +147,10 @@ e.preventDefault();
                   fill="currentColor"
                   d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
                 ></path>
-              </svg> 
-              ) : "Login"
-            }
+              </svg>
+            ) : (
+              "Login"
+            )}
           </motion.button>
         </form>
 
